af931de3bd3dafdad9ec5af7a95e24cd5f0be4c1
imjmt <yijunmin@gmail.com>
2021-12-27 16:57:08 +0900

    mock 데이터에 id 파트 수정

--- src/data/dummyProduct.ts
+++
@@ -1,1 +0,0 @@
-import {Product} from './api';
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -7,7 +7,18 @@ import {makeProductList} from '../data/api';
 const {width} = Dimensions.get('window');
 
 export const Home = function () {
-  const [productList] = useState<Product[]>(makeProductList(10));
+  const [productList, setProduectList] = useState<Product[]>(
+    makeProductList(20),
+  );
+  const onEndReached = () => {
+    const add = makeProductList(20);
+    setProduectList(() => productList.concat(add));
+  };
+  const onRefresh = () => {
+    setProduectList(() => makeProductList(20));
+  };
+  const [isRefreshing] = useState<boolean>(false);
+
   return (
     <View>
       <FlatList
@@ -16,6 +27,14 @@ export const Home = function () {
         renderItem={({item}) => <HomeProductList product={item} />}
         keyExtractor={(item, _index) => String(item.id)}
         ItemSeparatorComponent={() => <View style={[styles.itemSeparator]} />}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        windowSize={5}
+        onEndReached={onEndReached}
+        onEndReachedThreshold={0.6}
+        refreshing={isRefreshing}
+        onRefresh={onRefresh}
+        removeClippedSubviews={true}
       />
     </View>
   );
@@ -25,6 +44,7 @@ const styles = StyleSheet.create({
   flatList: {
     flex: 1,
     width: width,
+    padding: 15,
   },
-  itemSeparator: {borderWidth: 1, borderColor: 'black'},
+  itemSeparator: {borderWidth: 0.5, borderColor: 'lightgrey'},
 });
