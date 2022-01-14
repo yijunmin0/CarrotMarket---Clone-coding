197c779410b6a362cd35d37ccb9cea513cfa5ede
imjmt <yijunmin@gmail.com>
2022-01-14 14:55:13 +0900

    스크롤 업 시 하단뷰 내려감, 이미지 확대 등 구현

--- src/screens/productSpecific.tsx
+++ src/screens/productSpecific.tsx
@@ -1,13 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {useIsFocused, useNavigation} from '@react-navigation/native';
-import {StyleSheet, StatusBar, Platform} from 'react-native';
+import {StyleSheet, StatusBar, Platform, Animated} from 'react-native';
 import {Image} from 'react-native';
 import {Text} from '../assets/styles/Text';
 import {Header} from '../components/Header';
 import {ProductSpecificProps} from '../navigations/HomeStack';
 import Icon from 'react-native-vector-icons/AntDesign';
 import {Dimensions} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
 import {View} from '../assets/styles/View';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 
@@ -17,6 +16,15 @@ export const ProductSpecific = function ({route}: ProductSpecificProps) {
   const {id, picture, price, title, location} = route.params;
   const isFocused = useIsFocused();
   const navigation = useNavigation();
+  const scrolling = useRef(new Animated.Value(0)).current;
+  const translationView = scrolling.interpolate({
+    inputRange: [-200, 0, 0],
+    outputRange: [200, 0, 0],
+  });
+  const translationImage = scrolling.interpolate({
+    inputRange: [-400, -50, -50],
+    outputRange: [2.5, 1, 1],
+  });
   const [dark, setDark] = useState(false);
   return (
     <View style={styles.view}>
@@ -45,24 +53,53 @@ export const ProductSpecific = function ({route}: ProductSpecificProps) {
         }}
         dark={dark}
       />
-      <ScrollView
-        scrollEventThrottle={16}
+      <Animated.ScrollView
+        style={{
+          flex: 1,
+          transform: [{translateY: Animated.multiply(translationView, -1)}],
+        }}
+        scrollEventThrottle={1}
         onScroll={event => {
-          const scrolling = event.nativeEvent.contentOffset.y;
-          if (scrolling > Dimensions.get('window').width * 0.7) {
+          scrolling.setValue(event.nativeEvent.contentOffset.y);
+          if (
+            event.nativeEvent.contentOffset.y >
+            Dimensions.get('window').width * 0.7
+          ) {
             setDark(true);
           } else {
             setDark(false);
           }
         }}>
-        <Image source={{uri: picture}} style={styles.image} />
-        <Text>{id}</Text>
-        <Text>{price}</Text>
-        <Text>{title}</Text>
-        <Text>{location}</Text>
-        <Text>dummy</Text>
-        <View style={styles.spaceView} />
-      </ScrollView>
+        <Animated.Image
+          source={{uri: picture}}
+          style={{
+            ...styles.image,
+            transform: [{scale: translationImage}],
+          }}
+        />
+        <Animated.View
+          style={{
+            backgroundColor: 'white',
+            transform: [{translateY: translationView}],
+            top: -100,
+          }}>
+          <Text>{id}</Text>
+          <Text>{price}</Text>
+          <Text>{title}</Text>
+          <Text>{location}</Text>
+          <Text>dummy</Text>
+          <View style={styles.spaceView} />
+        </Animated.View>
+      </Animated.ScrollView>
+
+      {/* <Animated.ScrollView
+        style={styles.scrollView}
+        scrollEventThrottle={16}
+        event.nativeEvent.contentOffset.y
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrolling}}}],
+          {useNativeDriver: true},
+        )}></Animated.ScrollView> */}
     </View>
   );
 };
@@ -79,7 +116,6 @@ const styles = StyleSheet.create({
   spaceView: {
     width: '100%',
     height: 1000,
-    backgroundColor: 'skyblue',
   },
   header: {
     position: 'absolute',
@@ -88,6 +124,6 @@ const styles = StyleSheet.create({
   },
   image: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').width,
+    height: Dimensions.get('window').width + 100,
   },
 });
