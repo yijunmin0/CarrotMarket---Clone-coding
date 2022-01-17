d2f6beca0697aa437fedf99b79dd1d472fcb618c
imjmt <yijunmin@gmail.com>
2022-01-17 22:37:12 +0900

    singTap, DoubleTap, Loading 추가

--- src/screens/productSpecific.tsx
+++ src/screens/productSpecific.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {useIsFocused, useNavigation} from '@react-navigation/native';
 import {StyleSheet, StatusBar, Platform, Animated} from 'react-native';
 import {Image} from 'react-native';
@@ -9,15 +9,22 @@ import Icon from 'react-native-vector-icons/AntDesign';
 import {Dimensions} from 'react-native';
 import {View} from '../assets/styles/View';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
+import {
+  PanGestureHandler,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
 
 const os = Platform.OS;
 const standard = 40;
 
 export const ProductSpecific = function ({route}: ProductSpecificProps) {
-  const {id, picture, price, title, location} = route.params;
+  const {picture, price, title, location} = route.params;
   const isFocused = useIsFocused();
   const navigation = useNavigation();
+  const doubleTapRef = useRef();
+  const [isLike, setIsLike] = useState<boolean>(false);
   const scrolling = useRef(new Animated.Value(0)).current;
+  const opacity = useRef(new Animated.Value(1)).current;
   const translationView = scrolling.interpolate({
     inputRange: [-200, 0, 0],
     outputRange: [100, 0, 0],
@@ -27,7 +34,23 @@ export const ProductSpecific = function ({route}: ProductSpecificProps) {
     inputRange: [-400, -standard, -standard],
     outputRange: [2.8, 1, 1],
   });
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState<boolean>(false);
+  const [onLoad, setOnLoad] = useState<boolean>(false);
+  useEffect(() => {
+    !onLoad &&
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(opacity, {
+            toValue: 0.25,
+            useNativeDriver: true,
+          }),
+          Animated.timing(opacity, {
+            toValue: 0.75,
+            useNativeDriver: true,
+          }),
+        ]),
+      ).start();
+  });
   return (
     <View style={styles.view}>
       <StatusBar
@@ -56,11 +79,6 @@ export const ProductSpecific = function ({route}: ProductSpecificProps) {
         dark={dark}
       />
       <Animated.ScrollView
-        style={
-          {
-            // transform: [{translateY: Animated.multiply(translationView, -1.05)}],
-          }
-        }
         scrollEventThrottle={1}
         onScroll={event => {
           scrolling.setValue(event.nativeEvent.contentOffset.y);
@@ -73,37 +91,52 @@ export const ProductSpecific = function ({route}: ProductSpecificProps) {
             setDark(false);
           }
         }}>
-        <Animated.Image
-          source={{uri: picture}}
-          style={{
-            ...styles.image,
-            top: -standard,
-            transform: [{scale: translationImage}],
-          }}
-        />
-        <Animated.View
-          style={{
-            backgroundColor: 'white',
-            transform: [{translateY: translationView}],
-            top: -2 * standard,
+        <TapGestureHandler
+          waitFor={doubleTapRef}
+          onActivated={() => {
+            console.log(1);
           }}>
-          <Text>{id}</Text>
-          <Text>{price}</Text>
-          <Text>{title}</Text>
-          <Text>{location}</Text>
-          <Text>dummy</Text>
+          <TapGestureHandler
+            ref={doubleTapRef}
+            numberOfTaps={2}
+            maxDelayMs={300}
+            onActivated={() => {
+              setIsLike(isLike => !isLike);
+            }}>
+            <Animated.Image
+              source={{uri: picture}}
+              style={{
+                ...styles.image,
+                top: -standard,
+                transform: [{scale: translationImage}],
+              }}
+              onLoad={() => setOnLoad(true)}
+            />
+          </TapGestureHandler>
+        </TapGestureHandler>
+        {!onLoad ? (
+          <Animated.View
+            style={[styles.loadingImg, {top: -standard, opacity: opacity}]}
+          />
+        ) : null}
+        <Animated.View
+          style={[
+            styles.bottomView,
+            {transform: [{translateY: translationView}], top: -2 * standard},
+          ]}>
+          <View style={styles.titlebox}>
+            <Text style={styles.title}>{title}</Text>
+            {isLike ? (
+              <Icon name="star" size={20} />
+            ) : (
+              <Icon name="staro" size={20} />
+            )}
+          </View>
+          <Text>가격 : {price}</Text>
+          <Text>지역 : {location}</Text>
           <View style={styles.spaceView} />
         </Animated.View>
       </Animated.ScrollView>
-
-      {/* <Animated.ScrollView
-        style={styles.scrollView}
-        scrollEventThrottle={16}
-        event.nativeEvent.contentOffset.y
-        onScroll={Animated.event(
-          [{nativeEvent: {contentOffset: {y: scrolling}}}],
-          {useNativeDriver: true},
-        )}></Animated.ScrollView> */}
     </View>
   );
 };
@@ -122,6 +155,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 1000,
   },
+  bottomView: {backgroundColor: 'white'},
   header: {
     position: 'absolute',
     top: os === 'ios' ? getStatusBarHeight(true) : StatusBar.currentHeight,
@@ -130,4 +164,21 @@ const styles = StyleSheet.create({
   image: {
     height: Dimensions.get('window').width + 2 * standard,
   },
+  loadingImg: {
+    height: Dimensions.get('window').width + 2 * standard,
+    width: Dimensions.get('window').width,
+    backgroundColor: 'gray',
+    position: 'absolute',
+  },
+  titlebox: {
+    flex: 1,
+    padding: 10,
+    paddingRight: 20,
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'lightgray',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {fontSize: 25, fontWeight: '400'},
 });
